5b72acdc2152f87e6405328ad1b4952e9a0fb79a
Sakshi Saxena <sakshisaxena0808@gmail.com>
2022-09-05 19:02:28 +0530

    modified data

--- lib/Home.dart
+++ lib/Home.dart
@@ -253,7 +253,7 @@ class _HomePageState extends State<HomePage> {
                                         Container(
                                           padding: EdgeInsets.symmetric(horizontal: 200),
                                           child: Text(
-                                            "A pre-final year engineering student who loves to explore new horizons including open-source contribution and wishes to create an impact and encourage the community.",
+                                            "I'm a final year engineering student who loves to explore new horizons including open-source contribution and wishes to create an impact and encourage the community.",
                                             textAlign: TextAlign.center,
                                             style: TextStyle(
                                               color: Color(0xffCCD6F6).withOpacity(0.6),
